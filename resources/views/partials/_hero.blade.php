299f3507288584b1740687d13ad86219341c0236
Ermal Dobratiqi <ermaldobratiqi@hotmail.com>
2023-07-05 00:48:49 +0200

    Logo Updates

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -12,7 +12,7 @@
                 <div>
                     <a href="register.html"
                         class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
-                        Up to List a Gig</a>
+                        Up to List a Post</a>
                 </div>
             </div>
         </section>
